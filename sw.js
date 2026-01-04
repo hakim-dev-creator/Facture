1ae5420530920f90e16b9c95b8937eab903fb1b5
hakim-dev-creator <hakim-dev-creator@jorjeomomo.com>
2026-01-04 07:16:39 +0100

    fix offline and keys

---
+++ sw.js
@@ -0,0 +1,23 @@
+const cacheName = 'facture-v1';
+const assets = [
+  './',
+  './index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
